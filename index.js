33bc59bfb82d1b1be6df60ad03aa35438410fc29
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-06-02 21:42:50 +0530

    routes basics

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
+import authRoute from "./routes/authRoutes.js";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
+
+app.use("/api/auth", authRoute);
 
 app.listen(8000, () => {
   console.log("Server is running on PORT 8000");
