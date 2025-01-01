7adfdc3ef656f9fec6cea69503f967898d440e46
Tuhin Poddar <tuhinpoddar114@gmail.com>
2025-01-01 21:41:54 +0530

    updated

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoute from "./routes/authRoutes.js";
+import connectMongoDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
+// Middleware to parse JSON request bodies
+app.use(express.json()); // for parsing application/json
+
+// Middleware to parse URL-encoded request bodies (if you're using form data)
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
 app.use("/api/auth", authRoute);
 
 app.listen(8000, () => {
   console.log("Server is running on PORT 8000");
+  connectMongoDB();
 });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,29 +1,10 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/authController.js";
+
 const router = express.Router();
 
-// Define a GET route for the signup endpoint
-router.get("/signup", (req, res) => {
-  // Return a JSON response indicating that the signup endpoint was hit
-  res.json({
-    data: "You hit signup endpoint",
-  });
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-// Define a GET route for the login endpoint
-router.get("/login", (req, res) => {
-  // Return a JSON response indicating that the login endpoint was hit
-  res.json({
-    data: "You hit login endpoint",
-  });
-});
-
-// Define a GET route for the logout endpoint
-router.get("/logout", (req, res) => {
-  // Return a JSON response indicating that the logout endpoint was hit
-  res.json({
-    data: "You hit logout endpoint",
-  });
-});
-
-// Export the router instance as the default export
 export default router;
